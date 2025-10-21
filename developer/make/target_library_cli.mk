61a2bd0732eced586b3a455b6ab7cf4577b52696
Thomas Walker Lynch <eknp9n@reasoningtechnology.com>
2025-10-21 08:54:41 +0000

    .

--- developer/make/target_library_cli.mk
+++ developer/make/target_library_cli.mk
@@ -79,7 +79,12 @@ NEED_LIB := $(strip $(OBJECT_LIB))
 LIB_ARG  := $(if $(NEED_LIB),$(LIBFILE),)   # expands to lib path only when needed
 
 .PHONY: library
-library: $(if $(NEED_LIB),$(LIBFILE),.remove_lib_if_exists)
+library: $(if $(NEED_LIB),$(LIBFILE),.remove_lib_if_exists
+
+.PHONY: .remove_lib_if_exists
+.remove_lib_if_exists:
+	@rm -f $(LIBFILE)
+)
 
 ifneq ($(NEED_LIB),)
 $(LIBFILE): $(OBJECT_LIB)
